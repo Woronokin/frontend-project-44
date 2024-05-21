c5d5d7ab5e4a42f1b22f1d7fa84764472ad59b65
Woronokin <woronokin@icloud.com>
2024-05-21 19:40:29 +0300

    step 6.1: create new  brain-calc game

---
+++ src/brain-calc.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import cli from '../src/cli.js';
+import readlineSync from 'readline-sync';
+
+const isEven = (num) => num % 2 === 0;
+
+const brainCalc = () => {
+    const userName = cli();
+    console.log('What is the result of the expression?');
+
+    const roundsCount = 3;
+    for (let i = 0; i < roundsCount; i += 1) {
+        const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+        const numberOne = randomNumber();
+        const numberTwo = randomNumber();
+
+        console.log(`Question: ${numberOne} + ${numberTwo}`);
+        const userAnswer = readlineSync.question('Your answer: ');
+        const correctAnswer = numberOne + numberTwo;
+
+        if (userAnswer !== correctAnswer) {
+            console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+            console.log(`Let's try again, ${userName}!`);
+            return;
+        }
+
+        console.log('Correct!');
+    }
+
+    console.log(`Congratulations, ${userName}!`);
+};
+
+export default brainCalc;
